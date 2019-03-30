45124792dc8290ba16c598b89e11cd5ed5880bca
MikHail <mxh5594@rit.edu>
2019-03-30 00:22:35 -0400

    changed up preprocessing and improved resize methodology
    Former-commit-id: 000771ef04a77ecf67aad691e10e920f593be13e

--- classify.py
+++ classify.py
@@ -6,6 +6,7 @@ from keras.models import model_from_yaml
 from matplotlib import pyplot as plt
 import numpy as np
 import pickle
+import radialNonsense
 
 
 def load_model(bin_dir):
@@ -81,32 +82,44 @@ def deskew(img):
     M = cv.getRotationMatrix2D(center, angle, 1.0)
     return cv.warpAffine(img, M, (w, h), flags=cv.INTER_CUBIC, borderMode=cv.BORDER_REPLICATE)
 
+def padto(shape,i):
+    height, width = i.shape
+    dheight, dwidth = shape
+    xpad = dwidth-width
+    xtuple = (int(xpad/2),xpad-int(xpad/2))
+    ypad = dheight - height
+    ytuple = (int(ypad/2),ypad-int(ypad/2))
+    return np.pad(i,(ytuple,xtuple),'constant',constant_values=(255))
 
 def process(img, minconf):
     blur = cv.medianBlur(cv.medianBlur(img, 3), 3)
     _, th2 = cv.threshold(blur, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
     th2 = cv.dilate(th2, np.ones((3, 3), dtype=np.uint8), iterations=1)
-    th2 = cv.erode(th2, np.ones((5, 5), dtype=np.uint8), iterations=1)
+    th2 = cv.erode(th2, np.ones((5, 5), dtype=np.uint8), iterations=2)
+    th2 = cv.dilate(th2, np.ones((5, 5), dtype=np.uint8), iterations=1)
     th2 = deskew(th2)
-    nb_components, _, stats, _ = cv.connectedComponentsWithStats(cv.bitwise_not(th2), connectivity=8)
+    nb_components, _, stats, centroids = cv.connectedComponentsWithStats(cv.bitwise_not(th2), connectivity=8)
+    stats = radialNonsense.radialJoin(img,stats,centroids)
     stats = sorted(stats, key=lambda x: (x[0], x[1]))
     bounds = to_bgr(th2)
     res = to_bgr(np.ones((th2.shape[0], th2.shape[1]), dtype=np.uint8) * 255)
     chars = []
     areas = 0
 
-    for i in range(1, nb_components):
+    for i in range(len(stats)):
         sleft, stop, swidth, sheight, sarea = stats[i][0], stats[i][1], stats[i][2], stats[i][3], stats[i][4]
         if sarea > 500:
             areas+=sarea
             # Crop the bounding box of the symbol y:y+h, x:x+w
             dbbox = th2[stop:(stop + sheight), sleft:(sleft + swidth)]
-            ci = np.invert(cv.resize(flood_fill(dbbox), (28, 28)))
+            scale = min(28.0/swidth,28.0/sheight)
+            ci = np.invert(padto((28,28),cv.resize(flood_fill(dbbox),(int(round(swidth*scale)),int(round(sheight*scale))))))
+            #ci = np.invert(cv.resize(flood_fill(dbbox), (28, 28)))
             chars.append(ci)
             ci = ci.reshape(1, 28, 28, 1).astype('float32') / 255
             result = model.predict(ci)
             conf = str(round(max(result[0]) * 100, 2))
-
+            # TODO: over prioritize math symbols
             if float(conf) > float(minconf):
                 index = int(np.argmax(result, axis=1)[0])
                 print("Pred= [" + mapping[index] + "] conf= " + conf + " area=" + str(sarea))
