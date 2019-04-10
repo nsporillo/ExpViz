51cc9a8f6539ed6cf0fbbe4f1bf8e2591ee9e31e
Andrew Searns <andrew@haleymarketing.com>
2019-04-10 15:44:58 -0400

    Fixed up CYK algorithm. Added two simple grammars.
    Former-commit-id: 2a6c2c6bf4830c201df36e9e2222ae84f2a72442

--- parse.py
+++ parse.py
@@ -1,3 +1,5 @@
+import sys
+import re
 
 """
 Grammar:
@@ -24,30 +26,125 @@ CNF Grammar:
     DOT -> .
 """
 
+
+class Unit:
+    def __init__(self, left, term):
+        self.left = left
+        self.term = term
+
+    def __str__(self):
+        return str(self.left) + " -> " + self.term
+
+
+class Production:
+    def __init__(self, left, first, second):
+        self.left = left
+        self.A = first
+        self.B = second
+
+    def __str__(self):
+        return str(self.left) + " -> " + str(self.A) + " " + str(self.B)
+
+
+class Grammar:
+    def __init__(self):
+        self.units = []
+        self.productions = []
+        self.nterms = dict()
+
+    def __len__(self):
+        return len(self.units) + len(self.productions)
+
+    def __str__(self):
+        res = "["
+        for unit in self.units:
+            res += str(unit) + ", "
+        for production in self.productions:
+            res += str(production) + ", "
+        res += "]"
+        return res
+
+    def add_unit(self, left, term):
+        self.units.append(Unit(left, term))
+
+    def add_production(self, left, first, second):
+        self.productions.append(Production(left, first, second))
+
+
+def init_grammar(file):
+    grammar = Grammar()
+    nonterms = grammar.nterms
+    min_idx = 0
+    with open(file) as f:
+        for line in f:
+            terms = line.strip().split()
+            i = 2
+            if terms[0] in nonterms:
+                idx = nonterms[terms[0]]
+            else:
+                idx = min_idx
+                min_idx += 1
+                nonterms[terms[0]] = idx
+            while i < len(terms):
+                if i+1 == len(terms) or terms[i+1] == "|":
+                    grammar.add_unit(idx, terms[i])
+                    i = i + 2
+                else:
+                    A = terms[i]
+                    B = terms[i+1]
+                    if A in nonterms:
+                        A = nonterms[A]
+                    else:
+                        nonterms[A] = min_idx
+                        A = min_idx
+                        min_idx += 1
+                    if B in nonterms:
+                        B = nonterms[B]
+                    else:
+                        nonterms[B] = min_idx
+                        B = min_idx
+                        min_idx += 1
+                    grammar.add_production(idx, A, B)
+                    i = i + 3
+    return grammar
+
+
+def pretty_print(parse_tree):
+    for slice in parse_tree:
+        for list in slice:
+            print(list)
+        print("")
+
 def cyk(input, grammar):
     r = len(grammar)
     n = len(input)
-    Parse = [[[False for i in range(0, r)] for j in range(0, n)] for k in range(0, n)]
-    for s in range(0, n):
+    parse_tree = [[[False for k in range(0, r)] for j in range(0, n)] for i in range(0, n)]
+    for i in range(0, n):
         for unit in grammar.units:
-            if unit.val == input[s]:
-                Parse[1, s, unit.idx] = True
-    for l in range (1, n):
-        for s in range(0, n-l+1):
-            for p in range(0, l):
+            # Terminals can now be matched by REGEX
+            if re.match(unit.term, input[i]):
+                parse_tree[i][i][unit.left] = True
+    for l in range(1, n):
+        for i in range(0, n-l):
+            j = i + l
+            for k in range(i, j):
                 for production in grammar.productions:
-                    if Parse[p, s, production.first.idx] and Parse[l-p, s+p, production.second.idx]:
-                        Parse[l, s, production.var.idx] = True
-    if Parse[n, 1, 1]:
+                    if parse_tree[i][k][production.A] and parse_tree[k+1][j][production.B]:
+                        parse_tree[i][j][production.left] = True
+    pretty_print(parse_tree)
+    if parse_tree[0][n-1][0]:
         return "Member"
     else:
-        return "Not a memeber"
+        return "Not a member"
+
 
 def parse():
-    print("hello world")
-    # TODO:
-    # 1) Sort symbols left to right by left edge
-    # 2)
+    grammar = init_grammar(sys.argv[1])
+    print(grammar)
+    line = input("")
+    while line != "quit":
+        print(cyk(line, grammar))
+        line = input("")
 
 if __name__=='__main__':
     parse()
