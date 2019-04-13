fdaf2ad00304fe5368eb52b465bd0ca46d01bddf
William Estey <wpe2632@mail.rit.edu>
2019-04-12 22:47:33 -0400

    Moved code into comprehensive functions
    Former-commit-id: db958c3df7bd6de9339e178b258853bb5ce0b65f

--- text_detection/find_function_template.py
+++ text_detection/find_function_template.py
@@ -5,29 +5,37 @@ import argparse
 import imutils
 import glob
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-t", "--template", required=True, help="Path to template image")
-ap.add_argument("-i", "--images", required=True, help="Path to images where template will be matched")
-ap.add_argument("-v", "--visualize", help="Flag indicating whether or not to visualize each iteration")
-args = vars(ap.parse_args())
+
+def main():
+	# construct the argument parser and parse the arguments
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-t", "--template", required=True, help="Path to template image")
+	ap.add_argument("-i", "--images", required=True, help="Path to images where template will be matched")
+	ap.add_argument("-v", "--visualize", help="Flag indicating whether or not to visualize each iteration", action="store_true")
+	args = vars(ap.parse_args())
+
+	cv2.namedWindow('Visualize', cv2.WINDOW_NORMAL)
+	cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
+	cv2.namedWindow('Template', cv2.WINDOW_NORMAL)
+
+	template = cv2.imread(args["template"])
+	template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+	template = cv2.Canny(template, 50, 200)
+
+	cv2.imshow("Template", template)
+
+		# loop over the images to find the template in
+	for imagePath in glob.glob(args["images"] + "/*.jpg"):
+		image = cv2.imread(imagePath)
+		iterative_template_match(template, image)
 
 
-cv2.namedWindow('Visualize', cv2.WINDOW_NORMAL)
-cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
-cv2.namedWindow('Template', cv2.WINDOW_NORMAL)
+def iterative_template_match(template, image):
 
-template = cv2.imread(args["template"])
-template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-template = cv2.Canny(template, 50, 200)
-(tH, tW) = template.shape[:2]
-cv2.imshow("Template", template)
+	(tH, tW) = template.shape[:2]
 
-# loop over the images to find the template in
-for imagePath in glob.glob(args["images"] + "/*.jpg"):
-	# load the image, convert it to grayscale, and initialize the
+	# convert the image to grayscale, and initialize the
 	# bookkeeping variable to keep track of the matched region
-	image = cv2.imread(imagePath)
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	found = None
  
@@ -49,15 +57,6 @@ for imagePath in glob.glob(args["images"] + "/*.jpg"):
 		result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
 		(_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
  
-		# check to see if the iteration should be visualized
-		if args.get("visualize", False):
-			# draw a bounding box around the detected region
-			clone = np.dstack([edged, edged, edged])
-			cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
-				(maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
-			cv2.imshow("Visualize", clone)
-			cv2.waitKey(0)
- 
 		# if we have found a new maximum correlation value, then update
 		# the bookkeeping variable
 		if found is None or maxVal > found[0]:
@@ -73,3 +72,11 @@ for imagePath in glob.glob(args["images"] + "/*.jpg"):
 	cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
 	cv2.imshow("Image", image)
 	cv2.waitKey(0)
+
+
+def get_equation(img, template_box):
+	pass
+
+
+if __name__ == '__main__':
+	main()
