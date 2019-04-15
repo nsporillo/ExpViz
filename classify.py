c1f3fcf3513b6c40cb56c183b8d059cbee805f7d
Nick Porillo <nsporillo@gmail.com>
2019-04-14 21:54:25 -0400

    Code cleanup
    Former-commit-id: 06c7d4fe9b8566d03fada88613a5dfa6755efff7

--- classify.py
+++ classify.py
@@ -9,8 +9,8 @@ from preprocessing import *
 model = None
 mapping = None
 
-lookup = {'\\doteq':'=', '\\neg':'-', '\\ast':'*',
-                                '[':'(', ']':')'}
+lookup = {'\\doteq': '=', '\\neg': '-', '\\ast': '*',
+          '[': '(', ']': ')'}
 
 
 def load_model(bin_dir):
@@ -45,6 +45,7 @@ def plot_images(imgs, title):
     plt.axis('off')
     columns, rows = 4, 5
     ax = []
+
     for i in range(0, min(len(imgs), 20)):
         ax.append(fig.add_subplot(rows, columns, i + 1))
         img = cv.applyColorMap(imgs[i], cv.COLORMAP_BONE)
@@ -53,69 +54,80 @@ def plot_images(imgs, title):
 
 
 def process(img, minconf, debug=False):
+    """ Preprocessing techniques """
+    # 1) Twice median blur
+    # 2) Binary Threshold
+    # 3) Twice erosion with 5x5 kernel
+    # 4) Deskew with Hough Transform
     blur = cv.medianBlur(cv.medianBlur(img, 3), 3)
     _, th2 = cv.threshold(blur, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-    #th2 = cv.dilate(th2, np.ones((3, 3), dtype=np.uint8), iterations=1)
     th2 = cv.erode(th2, np.ones((5, 5), dtype=np.uint8), iterations=2)
-    #th2 = cv.dilate(th2, np.ones((5, 5), dtype=np.uint8), iterations=1)
     th2 = deskew(th2)
+
+    # Connected components, Radial Join, and sort components left to right
     nb_components, _, stats, centroids = cv.connectedComponentsWithStats(cv.bitwise_not(th2), connectivity=8)
-    stats = radialJoin(img, stats, centroids)
-    stats = sorted(stats, key=lambda x: (x[0], x[1]))
+    stats = sorted(radialJoin(img, stats, centroids), key=lambda x: (x[0], x[1]))
+
     bounds = to_bgr(th2)
     res = to_bgr(np.ones((th2.shape[0], th2.shape[1]), dtype=np.uint8) * 255)
     chars = []
     areas = 0
     equation = ""
+
     for i in range(len(stats)):
         sleft, stop, swidth, sheight, sarea = stats[i][0], stats[i][1], stats[i][2], stats[i][3], stats[i][4]
         if sarea > 500:
             areas += sarea
             # Crop the bounding box of the symbol y:y+h, x:x+w
-            dbbox = th2[(stop):(stop + sheight), (sleft):(sleft + swidth)]
+            dbbox = th2[stop:(stop + sheight), sleft:(sleft + swidth)]
             scale = min(28.0 / swidth, 28.0 / sheight)
-            ci = np.invert(padto((28, 28), cv.resize(flood_fill(dbbox),
-                                                     (int(round(swidth * scale)), int(round(sheight * scale))))))
-            # ci = np.invert(cv.resize(flood_fill(dbbox), (28, 28)))
+            pad = padto((28, 28),
+                        cv.resize(flood_fill(dbbox), (int(round(swidth * scale)), int(round(sheight * scale)))))
+            ci = np.invert(pad)
+
             chars.append(ci)
             ci = ci.reshape(1, 28, 28, 1).astype('float32') / 255
             result = model.predict(ci)
             conf = str(round(max(result[0]) * 100, 2))
-            # TODO: over prioritize math symbols
+
             if float(conf) > float(minconf):
                 index = int(np.argmax(result, axis=1)[0])
                 print("Pred= [" + mapping[index] + "] conf= " + conf + " area=" + str(sarea))
                 c = mapping[index]
+
                 if mapping[index] in lookup:
                     c = lookup[mapping[index]]
                 equation += c
+
                 if debug:
                     cv.putText(res, mapping[index], (sleft, stop), cv.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 3)
             if debug:
                 cv.rectangle(bounds, (sleft, stop), (sleft + swidth, stop + sheight), (0, 0, 255), thickness=2)
 
     if debug:
-        print('Average area: ' + str(areas / (nb_components - 1)))
-
+        print('Average Symbol Area: ' + str(areas / (nb_components - 1)))
         bounds = cv.resize(bounds, None, fx=0.5, fy=0.5)
         res = cv.resize(res, None, fx=0.5, fy=0.5)
         plt.axis("off")
         plt.imshow(np.vstack((bounds, res)))
         plot_images(chars, 'Chars')
+
     return equation
 
+
 def primer():
-    global model,mapping
+    global model, mapping
     model = load_model('bin')
     mapping = pickle.load(open('bin/mapping.p', 'rb'))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Classify symbols in an image.')
     parser.add_argument('-f', '--file', type=str, help='Image file', required=True)
-    parser.add_argument('-mc', '--minconf', type=str, default='5.0', help='Minimum Confidence Threshold')
+    parser.add_argument('-mc', '--minconf', type=str, default='30.0', help='Minimum Confidence Threshold')
     args = parser.parse_args()
 
     # Load trained model and classify the given image
     model = load_model('bin')
     mapping = pickle.load(open('bin/mapping.p', 'rb'))
-    process(load(args.file), minconf=args.minconf)
+    process(load(args.file), minconf=args.minconf, debug=True)
